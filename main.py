b78f28cf1ceff79ec9fe7318fa15170f24446e8b
Maxim Valov <50531941+MaximValov@users.noreply.github.com>
2026-08-31 10:44:29 +0300

    Update main.py
    test

--- main.py
+++ main.py
@@ -509,9 +509,9 @@ def main() -> None:
         resolved_cnn_path = first_existing_path(
             [
                 cnn_path,
-                "unet23jan_model.h5",
-                "models/unet23jan_model.h5",
-                "models/unet_model.h5",
+                # "unet23jan_model.h5",
+                "models/unet23jan_model2.h5",
+                # "models/unet_model.h5",
             ]
         )
 
